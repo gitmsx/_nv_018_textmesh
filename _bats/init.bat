bb95c10036a2e702eb345b67507d6225d6501d44
gitmsx <gitmsx@mail.ru>
2022-12-01 14:45:01 +0300

    Saved on 01.12.2022-14:44:49,43

---
+++ _bats/init.bat
@@ -0,0 +1,44 @@
+@echo off
+
+if not exist ".git" goto :already
+echo.
+echo    GIT already found !!!!!
+echo.
+echo    Exiting...
+goto :finished
+
+
+:already
+
+if not exist ".gitignore" goto :fail_message
+echo.
+echo    GIT ignore GOOD 
+echo.
+echo    Exiting...
+
+rem *************************
+rem *************************
+rem *************************
+
+
+echo "# _nv_018_textmesh" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gitmsx/_nv_018_textmesh.git
+git push -u origin main
+
+
+goto finished
+:fail_message
+echo.
+echo    Unfortunately GIT ignore not found
+echo.
+echo    Exiting...
+timeout 1 >nul
+goto finished        
+
+rem    ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+:finished
+exit
